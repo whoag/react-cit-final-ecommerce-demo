4a97aefb4aebc43b2d7bf6af79a86d3034552749
whoag <tarynthompson856@yahoo.com>
2022-03-30 13:24:42 -0400

    removed multer

--- react-cit-final/backend/server.mjs
+++ react-cit-final/backend/server.mjs
@@ -11,7 +11,7 @@ import bcrypt from 'bcrypt'
 import './config/passport.mjs'
 import User from "./models/usersModel.mjs";
 import cors from 'cors'
-import multer from 'multer'
+// import multer from 'multer'
 import Product from "./models/productModel.mjs";
 import * as fs from "fs";
 import * as path from "path";
@@ -34,25 +34,25 @@ app.use(function(req, res, next) {
 app.use(passport.initialize());
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const storage = multer.diskStorage({
-    //file destination
-    destination:function(req, file, callback){
-        callback(null, 'uploads')
-    },
-
-    filename: function (req, file, callback){
-        callback(null, Date.now()+file.name)
-    }
-});
-
-//upload params for multer
-const upload = multer({
-    storage:storage,
-    limits:{
-        fieldSize: 1024*1024*3
-    },
-})
+//
+// const storage = multer.diskStorage({
+//     //file destination
+//     destination:function(req, file, callback){
+//         callback(null, 'uploads')
+//     },
+//
+//     filename: function (req, file, callback){
+//         callback(null, Date.now()+file.name)
+//     }
+// });
+//
+// //upload params for multer
+// const upload = multer({
+//     storage:storage,
+//     limits:{
+//         fieldSize: 1024*1024*3
+//     },
+// })
 
 //Creating API for user
 app.use('/api/users', userRoutes)
@@ -61,28 +61,28 @@ app.use('/api/products', productRoutes)
 app.use('/api/wishlist', wishlistRoutes)
 
 
-app.post('/api/products', upload.single('image'), async (req, res)=>{
-    console.log(req)
-    // let product = {
-    //     name: req.body.name,
-    //     description: req.body.description,
-    //     price: req.body.price,
-    //     category_id: req.body.category_id,
-    //     image: {
-    //         data: fs.readFileSync(path.join('/api/products' + '/uploads/' +  req.data.body.image[0].name)),
-    //         contentType: req.data.body.image[0].type
-    //     }
-    // }
-    // await Product.create(product, (err, item) => {
-    //     if (err) {
-    //         return res.status(500).json({error: "Error adding product"});
-    //
-    //     } else {
-    //         // item.save();
-    //         return res.status(200).json({ error: "Product added" });
-    //     }
-    // })
-})
+// app.post('/api/products', upload.single('image'), async (req, res)=>{
+//     console.log(req)
+//     // let product = {
+//     //     name: req.body.name,
+//     //     description: req.body.description,
+//     //     price: req.body.price,
+//     //     category_id: req.body.category_id,
+//     //     image: {
+//     //         data: fs.readFileSync(path.join('/api/products' + '/uploads/' +  req.data.body.image[0].name)),
+//     //         contentType: req.data.body.image[0].type
+//     //     }
+//     // }
+//     // await Product.create(product, (err, item) => {
+//     //     if (err) {
+//     //         return res.status(500).json({error: "Error adding product"});
+//     //
+//     //     } else {
+//     //         // item.save();
+//     //         return res.status(200).json({ error: "Product added" });
+//     //     }
+//     // })
+// })
 app.post('/api/register', jsonParser, function (req, res)  {
     console.log(req.body)
     // Form validationconst { errors, isValid } = validateRegisterInput(req.body);// Check validation
