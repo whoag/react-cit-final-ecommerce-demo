c23e26009abb1211bd541c02d22fc2f104864b82
whoag <tarynthompson856@yahoo.com>
2022-04-24 14:52:22 -0400

    for desktop

--- react-cit-final/backend/controllers/wishlistController.mjs
+++ react-cit-final/backend/controllers/wishlistController.mjs
@@ -10,15 +10,15 @@ export const getWishlist = asyncHandler(async(req, res) => {
 
 //getUserById function to retrieve user by id
 export const getWishlistById  = asyncHandler(async(req, res) => {
-    const user = await Wishlist.findById(req.params.id)
+    const wish = await Wishlist.findById(req.params.id)
 
     //if user id match param id send user else throw error
-    if(user){
-        res.json(user)
+    if(wish){
+        res.json(wish)
     }else{
-        res.status(404).json({message: "User not found"})
+        res.status(404).json({message: "Wishlist not found"})
         res.status(404)
-        throw new Error('User not found')
+        throw new Error('Wishlist not found')
     }
 })
 
